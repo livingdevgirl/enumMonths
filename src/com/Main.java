003af03c3a16d0ff0cfbf505396047c0ca3910fc
Emma Dean <emmadeannow@gmail.com>
2017-09-08 00:31:57 -0400

    working and displaying the months in the correct format

--- src/com/Main.java
+++ src/com/Main.java
@@ -10,15 +10,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+
+        for (Month month : Month.values()
+                ) {
+
+            System.out.println((month.getEnglishName()));
+        }
         System.out.println("choose a month out of the year!");
         String userMonth = scanner.nextLine().toUpperCase();
-
-
-        //get the value of the user input and compare it to the value of the month get
-
-        //user month == month in month array?
-
-        //case statement
         for (Month month : Month.values()) {
             if (!userMonth.equals(month.getEnglishName().toUpperCase())) {
 
@@ -27,11 +27,22 @@ public class Main {
             }
 
         }
-    }
+        //get the value of the user input and compare it to the value of the month get
 
-    private List<Month> monthList;
+        //user month == month in month array?
 
-    public List<Month> getMonths(){
-        return monthList;
+        //case statement
+//        for (Month month : Month.values()) {
+//
+//
+////           String userMonth = scanner.nextLine().toUpperCase();
+//
+//    }
+//
+//        private List<Month> monthList;
+//
+//        public List<Month> getMonths () {
+//            return monthList;
+//        }
     }
 }
