a1768631d7f58bf9fe061f278c5c7c727a34df74
Emma Dean <emmadeannow@gmail.com>
2017-09-09 00:44:59 -0400

    working to print out the case statement and finished project

--- src/com/Main.java
+++ src/com/Main.java
@@ -12,26 +12,77 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-List<String> months = new ArrayList<>();
+        List<String> months = new ArrayList<>();
 
         for (Month month : Month.values()
                 ) {
 
 
-           months.add(month.getEnglishName());
+            months.add(month.getEnglishName().toUpperCase());
         }
+        for (int i = 0; i < months.indexOf(i); i++) {
+            System.out.println(months.indexOf(i));
+        }
+
         System.out.println(months);
         System.out.println("choose a month out of the year!");
         String userMonth = scanner.nextLine().toUpperCase();
         for (Month month : Month.values()) {
             if (!userMonth.equals(month.getEnglishName().toUpperCase())) {
             } else {
+
+                System.out.println(Month.valueOf(userMonth));
                 System.out.println("you have selected " + userMonth);
-                break;
+
+                    switch (month) {
+                        case JANUARY:
+                            System.out.println(" February, March, April, May, June, July, September, October, November and December are left in this current year.");
+                            break;
+                        case FEBRUARY:
+                            System.out.println(" March, April, May, June, July, September, October, November and December are left in this current year.");
+                            break;
+                        case MARCH:
+                            System.out.println(" April, May, June, July, September, October, November and December are left in this current year.");
+                            break;
+                        case APRIL:
+                            System.out.println(" May, June, July, September, October, November and December are left in this current year.");
+                            break;
+                        case MAY:
+                            System.out.println(" June, July, September, October, November and December are left in this current year.");
+                            break;
+                        case JUNE:
+                            System.out.println(" July, September, October, November and December are left in this current year.");
+                            break;
+                        case JULY:
+                            System.out.println(" August, September, October, November and December are left in this current year.");
+                            break;
+                        case AUGUST:
+                            System.out.println(" September, October, November and December are left in this current year.");
+                            break;
+                        case SEPTEMBER:
+                            System.out.println(" October, November and December are left in this current year.");
+                            break;
+                        case OCTOBER:
+                            System.out.println(" November and December are left in this current year.");
+                            break;
+                        case NOVEMBER:
+                            System.out.println(" December is the only month left in the current year.");
+                            break;
+                        case DECEMBER:
+                            System.out.println("this is the last month of the year");
+                            break;
+                    default:
+                        System.out.println("not a month");
+                    }
+                    }
+                }
+
                 //add the months for given month and use a switch statement to return that month and the months following in the year.
             }
 
         }
+
+
         //get the value of the user input and compare it to the value of the month get
 
         //user month == month in month array?
@@ -49,5 +100,3 @@ List<String> months = new ArrayList<>();
 //        public List<Month> getMonths () {
 //            return monthList;
 //        }
-    }
-}
